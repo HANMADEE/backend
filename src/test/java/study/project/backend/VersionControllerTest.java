94f519f54f0f0ebb72af398bc29c7cfdeebc12c6
Hyun Woo <hwsa10041@gmail.com>
2023-08-02 20:46:26 +0900

    test : 버전 테스트 추가

---
+++ src/test/java/study/project/backend/VersionControllerTest.java
@@ -0,0 +1,30 @@
+package study.project.backend;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+class VersionControllerTest {
+
+    @Autowired
+    private VersionController versionController;
+
+    @DisplayName("서버의 버전을 확인한다.")
+    @Test
+    void versionCheck() {
+        // given
+        // when
+        String version = versionController.serverVersionCheck();
+
+        // then
+        assertThat(version).isEqualTo("Version 0.0.1");
+    }
+}
