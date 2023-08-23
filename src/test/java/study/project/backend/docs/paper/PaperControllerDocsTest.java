a01bbcccbb7bb5a055cb21108cb0a2bcec1aeceb
Hyun Woo <hwsa10041@gmail.com>
2023-08-23 21:38:10 +0900

    docs : 롤링페이퍼 선물하기 API docs 작성

--- src/test/java/study/project/backend/docs/paper/PaperControllerDocsTest.java
+++ src/test/java/study/project/backend/docs/paper/PaperControllerDocsTest.java
@@ -3,6 +3,7 @@ package study.project.backend.docs.paper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import study.project.backend.docs.RestDocsSupport;
 import study.project.backend.domain.paper.controller.PaperController;
 import study.project.backend.domain.paper.request.PaperRequest;
@@ -10,6 +11,7 @@ import study.project.backend.domain.paper.response.PaperResponse;
 import study.project.backend.domain.paper.service.PaperService;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
@@ -19,6 +21,8 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.JsonFieldType.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.formParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -52,7 +56,7 @@ public class PaperControllerDocsTest extends RestDocsSupport {
         // when // then
         mockMvc.perform(
                         RestDocumentationRequestBuilders.post("/paper")
-                                .header("Authorization", "Bearer token")
+                                .header("Authorization", "Bearer Token")
                                 .content(objectMapper.writeValueAsString(request))
                                 .contentType(APPLICATION_JSON)
                 )
@@ -62,7 +66,7 @@ public class PaperControllerDocsTest extends RestDocsSupport {
                         preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()),
                         requestHeaders(
-                                headerWithName("Authorization").description("Bearer token")
+                                headerWithName("Authorization").description("Bearer Token")
                         ),
                         requestFields(
                                 fieldWithPath("subject").type(STRING).description("주제"),
@@ -83,4 +87,50 @@ public class PaperControllerDocsTest extends RestDocsSupport {
                 ));
     }
 
+    @DisplayName("롤링페이퍼 선물하기 API")
+    @Test
+    void giftRollingPaper() throws Exception {
+        // given
+        given(paperService.giftRollingPaper(anyLong(), anyLong(), any()))
+                .willReturn(
+                        PaperResponse.Create.builder()
+                                .id(1L)
+                                .userId(3L)
+                                .subject("생일")
+                                .theme("https://example.s3.ap-northeast-2.amazonaws.com/image/default.png")
+                                .isOpen(false)
+                                .isLikeOpen(false)
+                                .build()
+                );
+
+        // when // then
+        mockMvc.perform(
+                        RestDocumentationRequestBuilders.patch("/paper/gift")
+                                .header("Authorization", "Bearer Token")
+                                .param("paperId", "1")
+                                .param("giftedUserId", "3")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("giftRollingPaper",
+                        preprocessResponse(prettyPrint()),
+                        requestHeaders(
+                                headerWithName("Authorization").description("Bearer Token")
+                        ),
+                        formParameters(
+                                parameterWithName("paperId").description("롤링페이퍼 ID"),
+                                parameterWithName("giftedUserId").description("선물받을 유저 ID")
+                        ),
+                        responseFields(
+                                fieldWithPath("code").type(NUMBER).description("상태 코드"),
+                                fieldWithPath("message").type(STRING).description("상태 메세지"),
+                                fieldWithPath("data.id").type(NUMBER).description("롤링페이퍼 ID"),
+                                fieldWithPath("data.userId").type(NUMBER).description("롤링페이퍼 소유자 ID"),
+                                fieldWithPath("data.subject").type(STRING).description("주제"),
+                                fieldWithPath("data.theme").type(STRING).description("테마 이미지 URL"),
+                                fieldWithPath("data.isOpen").type(BOOLEAN).description("외부 공개 여부"),
+                                fieldWithPath("data.isLikeOpen").type(BOOLEAN).description("좋아요 공개 여부")
+                        )
+                ));
+    }
 }
