45a2bb7c377fc0261924f5d7cebc23b818c4104f
Hyun Woo <hwsa10041@gmail.com>
2023-08-29 13:35:58 +0900

    docs : 내 한마디 조회 API docs 작성

--- src/test/java/study/project/backend/docs/comment/CommentControllerDocsTest.java
+++ src/test/java/study/project/backend/docs/comment/CommentControllerDocsTest.java
@@ -6,12 +6,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import study.project.backend.docs.RestDocsSupport;
 import study.project.backend.domain.comment.controller.CommentController;
 import study.project.backend.domain.comment.request.CommentRequest;
 import study.project.backend.domain.comment.request.CommentServiceRequest;
 import study.project.backend.domain.comment.response.CommentResponse;
 import study.project.backend.domain.comment.service.CommentService;
+
+import java.util.List;
 
 import static com.epages.restdocs.apispec.ResourceDocumentation.parameterWithName;
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
@@ -47,7 +50,7 @@ public class CommentControllerDocsTest extends RestDocsSupport {
                 "생일축하해 한마디야!", "default.png", "godic", "center", "white", "친구"
         );
 
-        given(commentService.createComment(anyLong(),any(CommentServiceRequest.Create.class),any()))
+        given(commentService.createComment(anyLong(), any(CommentServiceRequest.Create.class), any()))
                 .willReturn(
                         CommentResponse.Create.builder()
                                 .id(1L)
@@ -112,4 +115,60 @@ public class CommentControllerDocsTest extends RestDocsSupport {
                         resource(parameters)));
     }
 
+    @DisplayName("내 한마디 조회 API")
+    @Test
+    void readMyComment() throws Exception {
+        // given
+        CommentResponse.Read response1 = new CommentResponse.Read(
+                1L, 1L, "한마딩", "생일축하해 한마딩아!!", "https://example.s3.ap-northeast-2.amazonaws.com/image/default.png",
+                "godic", "center", "white", "친구"
+        );
+
+        CommentResponse.Read response2 = new CommentResponse.Read(
+                8L, 2L, "두마딩", "생일축하해 두마딩아!!", "https://example.s3.ap-northeast-2.amazonaws.com/image/default.png",
+                "godic", "center", "white", "친구"
+        );
+
+        CommentResponse.Read response3 = new CommentResponse.Read(
+                21L, 3L, "세마딩", "생일축하해 세마딩아!!", "https://example.s3.ap-northeast-2.amazonaws.com/image/default.png",
+                "godic", "center", "white", "친구"
+        );
+
+        given(commentService.readMyComment(any()))
+                .willReturn(
+                        List.of(response1, response2, response3)
+                );
+
+        MockHttpServletRequestBuilder httpRequest = RestDocumentationRequestBuilders.get("/comment")
+                .header(AUTHORIZATION, "Bearer {token}");
+
+        ResourceSnippetParameters parameters = ResourceSnippetParameters.builder()
+                .tag("코멘트(한마디) API")
+                .summary("내 한마디 조회 API")
+                .requestHeaders(
+                        headerWithName("Authorization")
+                                .description("Swagger 요청시 해당 입력칸이 아닌 우측 상단 자물쇠 " +
+                                        "또는 Authorize 버튼을 이용해 토큰을 넣어주세요"))
+                .responseFields(
+                        fieldWithPath("code").type(NUMBER).description("상태 코드"),
+                        fieldWithPath("message").type(STRING).description("상태 메세지"),
+                        fieldWithPath("data[].id").type(NUMBER).description("코멘트 ID"),
+                        fieldWithPath("data[].paperId").type(NUMBER).description("남긴 페이퍼 ID"),
+                        fieldWithPath("data[].paperUserName").type(STRING).description("남긴 페이퍼 주인 이름"),
+                        fieldWithPath("data[].content").type(STRING).description("코멘트 내용"),
+                        fieldWithPath("data[].imageUrl").type(STRING).description("코멘트 이미지 URL"),
+                        fieldWithPath("data[].font").type(STRING).description("코멘트 폰트"),
+                        fieldWithPath("data[].sort").type(STRING).description("코멘트 정렬 방식"),
+                        fieldWithPath("data[].backgroundColor").type(STRING).description("코멘트 배경색"),
+                        fieldWithPath("data[].kind").type(STRING).description("코멘트 관계 유형"))
+                .build();
+
+        // when // then
+        mockMvc.perform(httpRequest)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("readMyComment",
+                        preprocessResponse(prettyPrint()),
+                        resource(parameters)));
+    }
 }
